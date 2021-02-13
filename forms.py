54ef1007562cde14701ef223f4f26ae657570fa8
Hewan <39294457+hewich@users.noreply.github.com>
2021-02-13 13:34:07 -0800

    created the form

---
+++ forms.py
@@ -0,0 +1,17 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, IntegerField
+from wtforms.validators import DataRequired, Email, ValidationError
+
+"""form to let existing user to login"""
+class LoginForm(FlaskForm):
+    username = StringField('User Name')
+    password = PasswordField('Password')
+    login = SubmitField('Login')
+
+
+"""form to let a user register an account"""
+class RegisterForm(FlaskForm):
+    username = StringField('User Name', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Submit')
